3302460a8de6f012a0593ad4520fce7c5cc378f9
Brandon <idhere703@gmail.com>
2017-04-04 18:52:53 -0400

    I like this bootstrap better.

--- src/App.js
+++ src/App.js
@@ -1,212 +1,203 @@
-import React, { Component } from 'react';
-import {
-  Navbar,
-  Nav,
-  NavItem,
-  Grid,
-  Row,
-  Col,
-  ListGroup,
-  ListGroupItem,
-  Thumbnail,
-  Button
- } from 'react-bootstrap'
+import React, {Component} from 'react';
 import './App.css';
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import socialLinks from './socialMediaLinks';
+import $ from 'jquery';
+
+// TODO: Put this in. Do it, do it now.
+// // Floating label headings for the contact form
+// $(function() {
+//     $("body").on("input propertychange", ".floating-label-form-group", function(e) {
+//         $(this).toggleClass("floating-label-form-group-with-value", !!$(e.target).val());
+//     }).on("focus", ".floating-label-form-group", function() {
+//         $(this).addClass("floating-label-form-group-with-focus");
+//     }).on("blur", ".floating-label-form-group", function() {
+//         $(this).removeClass("floating-label-form-group-with-focus");
+//     });
+// });
+//
+// // Navigation Scripts to Show Header on Scroll-Up
+// $(document).ready(function($) {
+//     var MQL = 1170;
+//
+//     //primary navigation slide-in effect
+//     if ($(window).width() > MQL) {
+//         var headerHeight = $('.navbar-custom').height();
+//         $(window).on('scroll', {
+//                 previousTop: 0
+//             },
+//             function() {
+//                 var currentTop = $(window).scrollTop();
+//                 //check if user is scrolling up
+//                 if (currentTop < this.previousTop) {
+//                     //if scrolling up...
+//                     if (currentTop > 0 && $('.navbar-custom').hasClass('is-fixed')) {
+//                         $('.navbar-custom').addClass('is-visible');
+//                     } else {
+//                         $('.navbar-custom').removeClass('is-visible is-fixed');
+//                     }
+//                 } else if (currentTop > this.previousTop) {
+//                     //if scrolling down...
+//                     $('.navbar-custom').removeClass('is-visible');
+//                     if (currentTop > headerHeight && !$('.navbar-custom').hasClass('is-fixed')) $('.navbar-custom').addClass('is-fixed');
+//                 }
+//                 this.previousTop = currentTop;
+//             });
+//     }
+// });
 
 class App extends Component {
-  getFoodContent = () => {
-    return (
-      <div>
-        <Grid>
-          <Row>
-            <Col xs={6} md={4}>
-              <Thumbnail src="http://placekitten.com/200/350" alt="242x200">
-                <h3>Cats...</h3>
-                <p>Not food. </p>
-                <p>
-
-
-                </p>
-              </Thumbnail>
-            </Col>
-            <Col xs={6} md={4}>
-              <Thumbnail src="http://placekitten.com/200/350" alt="242x200">
-                <h3>Who the heck designed this?</h3>
-                <p>I'm hungry!</p>
-                <p>
-
-
-                </p>
-              </Thumbnail>
-            </Col>
-            <Col xs={6} md={4}>
-              <Thumbnail src="http://placekitten.com/200/350" alt="242x200">
-                <h3>Perhaps I should use google maps.</h3>
-                <p>Then again I use that for everything. I feel like I overuse it.</p>
-                <p>
-
-
-                </p>
-              </Thumbnail>
-            </Col>
-            </Row>
-        </Grid>
-      </div>
-    );
-  };
-
-  getTravelContent = () => {
-    return (
-      <div>
-
-        <Grid>
-          <Row>
-            <Col xs={6} md={4}>
-              <Thumbnail src="http://placekitten.com/200/350" alt="242x200">
-                <h3>Where am I anyway...</h3>
-                <p>Looks strange.</p>
-                <p>
-
-
-                </p>
-              </Thumbnail>
-            </Col>
-            <Col xs={6} md={4}>
-              <Thumbnail src="http://placekitten.com/200/350" alt="242x200">
-                <h3>I suppose it really doesn't matter...</h3>
-                <p>Still, it's starting to freak me out.</p>
-                <p>
-
-
-                </p>
-              </Thumbnail>
-            </Col>
-            <Col xs={6} md={4}>
-              <Thumbnail src="http://placekitten.com/200/350" alt="242x200">
-                <h3>Help!!!!</h3>
-                <p>Don't panic, remember your training... Oh no! I don't have any training!</p>
-                <p>
-
-
-                </p>
-              </Thumbnail>
-            </Col>
-            </Row>
-        </Grid>
-      </div>
-    );
-  };
-
-  getHealthContent = () => {
-    return (
-      <div>
-        <Grid>
-          <Row>
-            <Col xs={6} md={4}>
-              <Thumbnail src="http://placekitten.com/200/350" alt="242x200">
-                <h3>What do you mean I'm not covered?!</h3>
-                <p>I had coverage just last week!</p>
-                <p>
-
-
-                </p>
-              </Thumbnail>
-            </Col>
-            <Col xs={6} md={4}>
-              <Thumbnail src="http://placekitten.com/200/350" alt="242x200">
-                <h3>I'll sue!</h3>
-                <p>There'll be hell to pay!</p>
-                <p>
-
-
-                </p>
-              </Thumbnail>
-            </Col>
-            <Col xs={6} md={4}>
-              <Thumbnail src="http://placekitten.com/200/350" alt="242x200">
-                <h3>... What do you mean I need coverage to sue!?</h3>
-                <p>This is ridiculous, I'm leaving. Where is the exit? ... I need coverage to use that too? I hate you all.</p>
-                <p>
-
-
-                </p>
-              </Thumbnail>
-            </Col>
-            </Row>
-        </Grid>
-      </div>
-    );
-  };
-
-  handleSelect = (index, last) => {
-    console.log('Selected tab: ' + index + ', Last tab: ' + last);
-  };
 
   render() {
     return (
       <div className="App">
 
-        <Grid>
-          <Row className="show-grid App-logo">
-            <Col>
-              <div className="App-header">
-                <Navbar bsStyle="inverse" collapseOnSelect>
-                  <Navbar.Header>
-                    <Navbar.Brand>
-                      <a href="#">(GTE) Got To Eat</a>
-                    </Navbar.Brand>
-                    <Navbar.Toggle />
-                  </Navbar.Header>
-                  <Navbar.Collapse>
-                    <Nav pullRight>
-                      {
-                        Object.keys(socialLinks).map((socialLink) => {
-                          console.log(socialLink);
-                          return (
-                            <NavItem href={socialLinks[socialLink].siteLink} onClick={() => {window.open(socialLinks[socialLink].siteLink);}}>
-                              <i className={`${socialLinks[socialLink].classInfo} social-links`} aria-hidden="true"></i>
-                            </NavItem>
-                          );
-                        })
-                      }
-                    </Nav>
-                  </Navbar.Collapse>
-                </Navbar>
+        <nav className="navbar navbar-default navbar-custom navbar-fixed-top">
+          <div className="container-fluid">
+            <div className="navbar-header page-scroll">
+              <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+                <span className="sr-only">Toggle navigation</span>
+                Menu
+                <i className="fa fa-bars"></i>
+              </button>
+              <a className="navbar-brand" href="index.html">Start Bootstrap</a>
+            </div>
 
-                <h2>Welcome!</h2>
-                <h3>Why is there a picture of space? Shut up.</h3>
+            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+              <ul className="nav navbar-nav navbar-right">
+                <li>
+                  <a href="index.html">Home</a>
+                </li>
+                <li>
+                  <a href="about.html">About</a>
+                </li>
+                <li>
+                  <a href="post.html">Sample Post</a>
+                </li>
+                <li>
+                  <a href="contact.html">Contact</a>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
 
+        <header className="intro-header header-image">
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                <div className="site-heading">
+                  <h1>Clean Blog</h1>
+                  <hr className="small"/>
+                  <span className="subheading">A Clean Blog Theme by Start Bootstrap</span>
+                </div>
               </div>
-            </Col>
+            </div>
+          </div>
+        </header>
 
-          </Row>
-        </Grid>
-
-        <div className="App-body">
-          <Tabs
-            onSelect={this.handleSelect}
-            selectedIndex={2}
-          >
-            <TabList>
-              {/* Actual tabs that you click on. */}
-              <Tab>Food</Tab>
-              <Tab>Health</Tab>
-              <Tab>Travel</Tab>
-            </TabList>
-            {/* Content for tabs. */}
-            <TabPanel>
-              {this.getFoodContent()}
-            </TabPanel>
-            <TabPanel>
-              {this.getHealthContent()}
-            </TabPanel>
-            <TabPanel>
-              {this.getTravelContent()}
-            </TabPanel>
-          </Tabs>
+        <div className="container">
+          <div className="row">
+            <div className="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+              <div className="post-preview">
+                <a href="post.html">
+                  <h2 className="post-title">
+                    Man must explore, and this is exploration at its greatest
+                  </h2>
+                  <h3 className="post-subtitle">
+                    Problems look mighty small from 150 miles up
+                  </h3>
+                </a>
+                <p className="post-meta">Posted by
+                  <a href="#">Start Bootstrap</a>
+                  on September 24, 2014</p>
+              </div>
+              <hr/>
+              <div className="post-preview">
+                <a href="post.html">
+                  <h2 className="post-title">
+                    I believe every human has a finite number of heartbeats. I don't intend to waste any of mine.
+                  </h2>
+                </a>
+                <p className="post-meta">Posted by
+                  <a href="#">Start Bootstrap</a>
+                  on September 18, 2014</p>
+              </div>
+              <hr/>
+              <div className="post-preview">
+                <a href="post.html">
+                  <h2 className="post-title">
+                    Science has not yet mastered prophecy
+                  </h2>
+                  <h3 className="post-subtitle">
+                    We predict too much for the next year and yet far too little for the next ten.
+                  </h3>
+                </a>
+                <p className="post-meta">Posted by
+                  <a href="#">Start Bootstrap</a>
+                  on August 24, 2014</p>
+              </div>
+              <hr/>
+              <div className="post-preview">
+                <a href="post.html">
+                  <h2 className="post-title">
+                    Failure is not an option
+                  </h2>
+                  <h3 className="post-subtitle">
+                    Many say exploration is part of our destiny, but it’s actually our duty to future generations.
+                  </h3>
+                </a>
+                <p className="post-meta">Posted by
+                  <a href="#">Start Bootstrap</a>
+                  on July 8, 2014</p>
+              </div>
+              <hr/>
+              <ul className="pager">
+                <li className="next">
+                  <a href="#">Older Posts &rarr;</a>
+                </li>
+              </ul>
+            </div>
+          </div>
         </div>
+
+        <hr/>
+
+        <footer>
+          <div className="container">
+            <div className="row">
+              <div className="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                <ul className="list-inline text-center">
+                  <li>
+                    <a href="#">
+                      <span className="fa-stack fa-lg">
+                        <i className="fa fa-circle fa-stack-2x"></i>
+                        <i className="fa fa-twitter fa-stack-1x fa-inverse"></i>
+                      </span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <span className="fa-stack fa-lg">
+                        <i className="fa fa-circle fa-stack-2x"></i>
+                        <i className="fa fa-facebook fa-stack-1x fa-inverse"></i>
+                      </span>
+                    </a>
+                  </li>
+                  <li>
+                    <a href="#">
+                      <span className="fa-stack fa-lg">
+                        <i className="fa fa-circle fa-stack-2x"></i>
+                        <i className="fa fa-github fa-stack-1x fa-inverse"></i>
+                      </span>
+                    </a>
+                  </li>
+                </ul>
+                <p className="copyright text-muted">Copyright &copy; Your Website 2016</p>
+              </div>
+            </div>
+          </div>
+        </footer>
+
       </div>
     );
   }
