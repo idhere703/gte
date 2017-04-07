529d6edfe6dae09069d5c4ee58d1cc4f05f7f812
Brandon <idhere703@gmail.com>
2017-04-07 17:39:22 -0400

    Now I just need the rest of them in there.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import './App.css';
 import $ from 'jquery';
 import SocialMediaLinks from './components/socialMedia';
+import BlogPosts from './components/blogPosts';
 
 // TODO: Put this in. Do it, do it now.
 // // Floating label headings for the contact form
@@ -98,20 +99,9 @@ class App extends Component {
         <div className="container">
           <div className="row">
             <div className="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-              <div className="post-preview">
-                <a href="post.html">
-                  <h2 className="post-title">
-                    Man must explore, and this is exploration at its greatest
-                  </h2>
-                  <h3 className="post-subtitle">
-                    Problems look mighty small from 150 miles up
-                  </h3>
-                </a>
-                <p className="post-meta">Posted by
-                  <a href="#">Start Bootstrap</a>
-                  on September 24, 2014</p>
-              </div>
-              <hr/>
+
+              <BlogPosts></BlogPosts>
+
               <div className="post-preview">
                 <a href="post.html">
                   <h2 className="post-title">
---
+++ src/components/blogPosts.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import blogData from '../data/blogposts';
+
+
+class BlogPosts extends Component {
+
+  render() {
+    return (
+      <div>
+        {blogData.map((post) => {
+          return (
+            <div>
+              <div className="post-preview">
+                <a href="post.html">
+                  <h2 className="post-title">
+                    {post.title}
+                  </h2>
+                  <h3 className="post-subtitle">
+                    {post.subtitle}
+                  </h3>
+                </a>
+                <p className="post-meta">Posted by
+                  <a href="#">{post.postedBy}</a>
+                  {post.postedAt}
+                </p>
+              </div>
+              <hr/>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default BlogPosts;
