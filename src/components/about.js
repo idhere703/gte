049f1975a79eb9134f19155912f8fcdc256e6e5c
Brandon <idhere703@gmail.com>
2017-04-20 22:24:53 -0400

    Added a timeline, well, it needs tweaking. I'll do that in the morning.

--- src/components/about.js
+++ src/components/about.js
@@ -1,38 +1,49 @@
 import React from 'react';
+import Timeline from './timeline';
 
 
 class About extends React.Component {
 
-    render() {
-        return (<div>
-                  <header className="intro-header about-header-image">
-                    <div className="container">
-                      <div className="row">
-                        <div className="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-                          <div className="page-heading">
-                            <h1>About Me</h1>
-                            <hr className="small" />
-                            <span className="subheading">This is what I do... I wonder what that man is staring at.</span>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </header>
-                  <div className="container">
-                    <div className="row">
-                      <div className="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
-                        <p>Hi there! I'm Brandon, a "full stack" developer (as if that means anything anymore) with about 5 years of experience in the field of software development.
-                          Native to SC living in Charleston for my entire life.</p>
-                        <p>In my free time you can find me outside exploring the world (pro tip, bring bug spray and sunscreen), hanging out with close friends and generally living
-                          a very fufilling and well rounded life... I'm not a crazy person, I swear. <i>said every crazy person ever</i></p>
-                        <p>If you would like to contact me, please don't use the contact page, as I haven't actually hooked up content for it yet. And I'll probably get rid of it.
-                          Try Linked instead.</p>
-                        <p>P.S: I got the theme for this blog from Start Bootstrap. Also that image came from them so if it's stolen I didn't do it.</p>
+  render() {
+    return (<div>
+              <header className="intro-header about-header-image">
+                <div className="container">
+                  <div className="row">
+                    <div className="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                      <div className="page-heading">
+                        <h1>About Me</h1>
+                        <hr className="small" />
+                        <span className="subheading">This is what I do... I wonder what that man is staring at.</span>
                       </div>
                     </div>
                   </div>
-                </div>);
-    }
+                </div>
+              </header>
+              <div className="container">
+                <div className="row">
+                  <div className="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                    <p>Hi there! I'm Brandon, a "full stack" developer (as if that means anything anymore) with about 5 years of experience in the field of software development.
+                      Native to SC living in Charleston for my entire life.</p>
+                    <p>In my free time you can find me outside exploring the world (pro tip, bring bug spray and sunscreen), hanging out with close friends and generally living
+                      a very fufilling and well rounded life... I'm not a crazy person, I swear. <i>said every crazy person ever</i></p>
+                    <p>If you would like to contact me, please don't use the contact page, as I haven't actually hooked up content for it yet. And I'll probably get rid of it.
+                      Try Linked instead.</p>
+                    <p>P.S: I got the theme for this blog from Start Bootstrap. Also that image came from them so if it's stolen I didn't do it.</p>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                    <h1>Timeline! Well, it's supposed to be anyway. I'll fix it.</h1>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                    <Timeline></Timeline>
+                  </div>
+                </div>
+              </div>
+            </div>);
+  }
 }
 
 export default About;
