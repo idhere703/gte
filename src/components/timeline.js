ea998043903c2d5f744a1bf4d2f3696ca7c32490
Brandon <idhere703@gmail.com>
2017-04-21 19:27:02 -0400

    Timeline is a little better.

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -4,23 +4,23 @@ import React from 'react';
 class Timeline extends React.Component {
 
     render() {
-        return (<div className="timeline__container">
-                  <div className="timeline-item" date-is='20-07-1990'>
+        return (<div>
+                  <div className="timeline-item" dateIs={ '20-07-1990' }>
                     <h1>Hello, 'Im a single div responsive timeline without mediaQueries!</h1>
                     <p>
                       I'm speaking with myself, number one, because I have a very good brain and I've said a lot of things. I write the best placeholder text, and I'm the biggest developer
                       on the web by far... While that's mock-ups and this is politics, are they really so different? I think the only card she has is the Lorem card.
                     </p>
                   </div>
-                  <div className="timeline-item" date-is='20-07-1990'>
+                  <div className="timeline-item" dateIs={ '20-07-1990' }>
                     <h1>Oeehhh, that's awesome.. Me too!</h1>
                     <p>
                       I'm speaking with myself, number one, because I have a very good brain and I've said a lot of things. I write the best placeholder text, and I'm the biggest developer
                       on the web by far... While that's mock-ups and this is politics, are they really so different? I think the only card she has is the Lorem card.
                     </p>
                   </div>
-                  <div className="timeline-item" date-is='20-07-1990'>
-                    <h1>I'm ::last-child so my border fades ^__^</h1>
+                  <div className="timeline-item" dateIs={ '20-07-1990' }>
+                    <h1>I'm ::last-child so my border fades ^__^ Or it's supposed to. Still needs fixin....</h1>
                     <p>
                       I'm speaking with myself, number one, because I have a very good brain and I've said a lot of things. I write the best placeholder text, and I'm the biggest developer
                       on the web by far... While that's mock-ups and this is politics, are they really so different? I think the only card she has is the Lorem card.
