72097e670a14af06d1d22df017dcee281fdb5a55
Brandon <idhere703@gmail.com>
2017-06-17 18:08:59 -0400

    Addeed individual comments.

--- src/components/comment.js
+++ src/components/comment.js
@@ -4,7 +4,18 @@ class Comment extends React.Component {
 
     render() {
         return (
-            <div></div>
+            <div>
+              <li className="comment user-comment">
+                <div className="info">
+                  <a href="#">this.props.comment.postedBy</a>
+                  <span>{ this.props.comment.postedAt }</span>
+                </div>
+                <a className="avatar" href="#">
+                  <img src={ this.props.comment.profilePicture } width="35" alt="Profile Avatar" title={ this.props.comment.postedBy } />
+                </a>
+                <p>this.props.comment.content</p>
+              </li>
+            </div>
             );
     }
 }
--- src/components/comments.js
+++ src/components/comments.js
@@ -1,63 +1,28 @@
 import React from 'react';
+import Comment from './comment';
 
 class Comments extends React.Component {
-    render() {
-        return (
-            <div>
-              <ul className="comment-section">
-                <li className="comment user-comment">
-                  <div className="info">
-                    <a href="#">Anie Silverston</a>
-                    <span>4 hours ago</span>
-                  </div>
-                  <a className="avatar" href="#">
-                    <img src="../img/avatar_user_1.jpg" width="35" alt="Profile Avatar" title="Anie Silverston" />
-                  </a>
-                  <p>Suspendisse gravida sem?</p>
-                </li>
-                <li className="comment author-comment">
-                  <div className="info">
-                    <a href="#">Jack Smith</a>
-                    <span>3 hours ago</span>
-                  </div>
-                  <a className="avatar" href="#">
-                    <img src="../img/avatar_author.jpg" width="35" alt="Profile Avatar" title="Jack Smith" />
-                  </a>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse gravida sem sit amet molestie portitor.</p>
-                </li>
-                <li className="comment user-comment">
-                  <div className="info">
-                    <a href="#">Bradley Jones</a>
-                    <span>1 hour ago</span>
-                  </div>
-                  <a className="avatar" href="#">
-                    <img src="../img/avatar_user_2.jpg" width="35" alt="Profile Avatar" title="Bradley Jones" />
-                  </a>
-                  <p>Suspendisse gravida sem sit amet molestie portitor?</p>
-                </li>
-                <li className="comment author-comment">
-                  <div className="info">
-                    <a href="#">Jack Smith</a>
-                    <span>1 hour ago</span>
-                  </div>
-                  <a className="avatar" href="#">
-                    <img src="../img/avatar_author.jpg" width="35" alt="Profile Avatar" title="Jack Smith" />
-                  </a>
-                  <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisee gravida sem sit amet molestie porttitor.</p>
-                </li>
-                <li className="write-new">
-                  <form>
-                    <textarea placeholder="Write your comment here" name="comment"></textarea>
-                    <div>
-                      <img src="../img/avatar_user_2.jpg" width="35" alt="Profile of Bradley Jones" title="Bradley Jones" />
-                      <button type="submit">Submit</button>
-                    </div>
-                  </form>
-                </li>
-              </ul>
-            </div>
-            );
-    }
+  render() {
+    return (
+      <div>
+        <ul className="comment-section">
+          { this.state.comments && this.state.comments.length &&
+            this.state.comments.map((comment) => {
+              return (<Comment comment={ comment }></Comment>);
+            }) }
+          <li className="write-new">
+            <form>
+              <textarea placeholder="Write your comment here" name="comment"></textarea>
+              <div>
+                <img src="../img/avatar_user_2.jpg" width="35" alt="Profile of Bradley Jones" title="Bradley Jones" />
+                <button type="submit">Submit</button>
+              </div>
+            </form>
+          </li>
+        </ul>
+      </div>
+      );
+  }
 }
 
 export default Comments;
