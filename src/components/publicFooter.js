7c8422f871f959e477b9a573bd2a1650d56647b7
Brandon <idhere703@gmail.com>
2017-08-29 19:07:45 -0400

    Updated footer copyright

--- src/components/publicFooter.js
+++ src/components/publicFooter.js
@@ -11,7 +11,7 @@ class PublicFooter extends React.Component {
                 <div className="row">
                   <div className="col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
                     <SocialMediaLinks></SocialMediaLinks>
-                    <p className="copyright text-muted">Copyright © fake blog 2017</p>
+                    <p className="copyright text-muted">Copyright © Brandon Underwood 2017</p>
                   </div>
                 </div>
               </div>
